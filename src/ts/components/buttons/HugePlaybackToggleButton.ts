60277398349d33ac9f99b52624cb172e686132a6
DidierF <didier.fuentes@gmail.com>
2025-11-17 16:28:43 -0500

    Fixes timeouts for mobile player.

--- src/ts/components/buttons/HugePlaybackToggleButton.ts
+++ src/ts/components/buttons/HugePlaybackToggleButton.ts
@@ -128,8 +128,10 @@ export class HugePlaybackToggleButton extends PlaybackToggleButton {
       player.seek(0);
       setTimeout(() => {
         player.play('ui');
-        player.pause('ui');
       }, 100);
+      setTimeout(() => {
+        player.pause('ui');
+      }, 200);
     })
 
     const suppressPlayButtonTransitionAnimation = () => {
